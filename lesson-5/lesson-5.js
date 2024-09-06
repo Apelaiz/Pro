ee8ad0a2134b96452b41074c8ad14cb415716c7e
Apelaiz <apelaiz@yandex.ru>
2024-09-06 16:43:10 +0300

    lesson-5

--- lesson-5/lesson-5.js
+++ lesson-5/lesson-5.js
@@ -1,86 +1,78 @@
-// let screenPrice = 10000
-// let percentage = 10 
-// let allServicePrices;
-// let newTitle = '';
-// let titleProject;
-// let screensValue;
-// let responsive;
+// Изначальные переменные
+let screenPrice = 10000;
+let percentage = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let newTitle = '';
+let titleProject;
+let screensValue;
+let responsive;
 
-
+// Получение данных через prompt
 const asking = function() {
-  titleProject = prompt ('Название проекта')
-  screensValue = prompt ('шаблонные, с уникальным дизайном, с анимациями')
-  responsive = prompt ('Нужен респонсив?')
-}
+  titleProject = prompt('Название проекта');
+  screensValue = prompt('Шаблонные, с уникальным дизайном, с анимациями');
+  responsive = prompt('Нужен респонсив?');
+};
 
+// Получение дополнительных услуг и их цен
+let service = prompt('Какой дополнительный вид услуги нужен?');
+let servicePrice = +prompt('Сколько это будет стоить?');
+let serviceSecond = prompt('Какой дополнительный вид услуги нужен?');
+let servicePriceSecond = +prompt('Сколько это будет стоить?');
 
-
-let service = prompt ('Какой дополнительный вид услуги нужен?')
-let servicePrice = +prompt ('Сколько это будет стоить?')
-let serviceSecond = prompt ('Какой дополнительный вид услуги нужен?')
-let servicePriceSecond = +prompt ('Сколько это будет стоить?')
-
-
+// Функция для расчета суммы всех услуг
 const getAllServicePrices = function() {
-    return servicePrice + serviceSecond
-}
+    return servicePrice + servicePriceSecond;
+};
 
-
+// Функция для расчета полной стоимости проекта
 function getFullPrice() {
-    return screenPrice + allServicePrices
+    return screenPrice + allServicePrices;
 }
 
+// Функция для расчета итоговой стоимости с учетом процента
+const getServicePercentPrices = function() {
+    return fullPrice - (fullPrice * (percentage / 100));
+};
 
-const getServicePercentPices = function () {
-    return fullPrice - (fullPrice * (percentage / 100))
-}
-
-
+// Функция для изменения названия проекта
 const getTitle = function() {
-    return titleProject[0].toUpperCase() + titleProject.slice(1).toLocaleLowerCase()
-}
+    return titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase();
+};
 
+// Функция для расчета скидки для клиента
+function getRollbackMessage() {
+    let fullPrice = getFullPrice();
 
-
-//Предоставление скидки
-function getRollbackMessage(fullPrice) {
     if (fullPrice > 50000) {
-        let percentage = 10;
-        console.log('Ваша скидка', percentage.toString(), 'процентов');
-    } else if (fullPrice > 20000 && fullPrice < 50000) {
-        let percentage = 5;
-        console.log('Ваша скидка', percentage.toString(), 'процентов');
-    } else if (fullPrice < 20000 && fullPrice > 0) {
+        console.log('Делаем скидку 10%');
+    } else if (fullPrice > 20000 && fullPrice <= 40000) {
+        console.log('Делаем скидку 5%');
+    } else if (fullPrice <= 20000 && fullPrice > 0) {
         console.log('Скидка не предусмотрена');
     } else if (fullPrice < 0) {
-        console.log('Что то пошло не так');
-    } else if (fullPrice === 0) {
-        console.log('Значение стоимости: 0');
-    } else if (fullPrice === 20000) {
-        console.log('Скидка не применяется к данной сумме');
-    } else if (fullPrice === 50000) {
-        console.log('Скидка не применяется к данной сумме');
-    } else {
-        console.log('Что то пошло не так');
+        console.log('Что-то пошло не так');
+    } else if (fullPrice === 0 || fullPrice === 20000 || fullPrice === 50000) {
+        console.log('Проверка на строгое равенство');
     }
 }
 
-
+// Вызовы функций
 asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
-servicePercentPrice = getAllServicePrices();
-getRollbackMessage(fullPrice);
-
+servicePercentPrice = getServicePercentPrices();
 newTitle = getTitle();
 
-
-console.log(service);
-console.log(titleProject);
-console.log(screensValue);
-console.log(responsive);
-console.log(servicePrice);
-console.log(serviceSecond);
-console.log(servicePriceSecond);
-console.log(Math.ceil(servicePercentPrice));
-console.log(getRollbackMessag);
+// Вывод результатов
+console.log(`Название проекта: ${newTitle}`);
+console.log(`Шаблонные, с уникальным дизайном, с анимациями: ${screensValue}`);
+console.log(`Нужен респонсив? ${responsive}`);
+console.log(`Дополнительная услуга: ${service}`);
+console.log(`Стоимость дополнительной услуги: ${servicePrice}`);
+console.log(`Дополнительная услуга (второе): ${serviceSecond}`);
+console.log(`Стоимость дополнительной услуги (второе): ${servicePriceSecond}`);
+console.log(`Стоимость проекта с учётом % подрядчику: ${Math.ceil(servicePercentPrice)} рублей`);
+getRollbackMessage();
